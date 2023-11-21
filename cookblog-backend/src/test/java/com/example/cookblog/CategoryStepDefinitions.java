d8dbb4d5c5a549da63a40a8cfccaf83acb3675e9
Sebastian Grosfeld <sebastian.grosfeld@wp.pl>
2023-11-22 00:52:18 +0100

    11-15

--- cookblog-backend/src/test/java/com/example/cookblog/CategoryStepDefinitions.java
+++ cookblog-backend/src/test/java/com/example/cookblog/CategoryStepDefinitions.java
@@ -1,6 +1,10 @@
 package com.example.cookblog;
 
+import com.example.cookblog.dto.requests.CreateCategoryRequest;
+import com.example.cookblog.dto.requests.UpdateCategoryRequest;
 import com.example.cookblog.model.Category;
+import com.example.cookblog.repository.CategoryRepository;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -14,8 +18,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,8 +35,11 @@ public class CategoryStepDefinitions {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     private Category category;
+    private UpdateCategoryRequest updateCategoryRequest;
     private ResultActions resultActions;
 
     @Given("I prepared category data")
@@ -66,4 +75,62 @@ public class CategoryStepDefinitions {
                 .andExpect(jsonPath("$[0].name").value(category.getName()));
     }
 
+    @Given("There are existing categories in the blog with title {string}")
+    public void thereAreExistingCategoriesInTheBlogWithTitle(String name) {
+        category = Category.builder()
+                .name(name)
+                .build();
+        categoryRepository.save(category);
+    }
+
+    @And("I prepared update category data")
+    public void iPreparedUpdateCategoryData() {
+        updateCategoryRequest = UpdateCategoryRequest
+                .builder()
+                .name("Updated")
+                .build();
+    }
+
+    @When("I update category")
+    public void iUpdateCategory() throws Exception {
+        final var content = objectMapper.writeValueAsString(updateCategoryRequest);
+        resultActions = mockMvc.perform(put("/categories/{id}", category.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content));
+        category = categoryRepository.findById(category.getId()).orElse(null);
+    }
+
+
+    @Then("I should see updated category")
+    public void iShouldSeeUpdatedCategory() throws Exception {
+        resultActions.andExpect(status().isNoContent());
+        assertEquals(category.getName(), "Updated");
+    }
+
+    @When("I delete the category")
+    public void iDeleteTheCategory() throws Exception {
+        resultActions = mockMvc.perform(delete("/categories/{id}", category.getId()))
+                .andExpect(status().isNoContent());
+    }
+
+    @Then("The category should no longer exist")
+    public void theCategoryShouldNoLongerExist() throws Exception {
+        mockMvc.perform(get("/categories/{id}", category.getId()))
+                .andExpect(status().isNotFound());
+    }
+
+    @When("I request to get the category by its ID {string}")
+    public void iRequestToGetTheCategoryByItsID(String name) throws Exception {
+        resultActions = mockMvc.perform(get("/categories/{id}", category.getId()))
+                .andExpect(status().isOk());
+    }
+
+    @Then("I should receive the details of the requested category")
+    public void iShouldReceiveTheDetailsOfTheRequestedCategory() throws Exception {
+        resultActions.andExpect(status().isOk())
+                .andExpectAll(
+                        jsonPath("$.id").exists(),
+                        jsonPath("$.name")
+                                .value(category.getName()));
+    }
 }
