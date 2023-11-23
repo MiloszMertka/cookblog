a68e1b248009f4f80bf3a757034d67ea2253e773
bborkowsp <pawel.borkowski0704@gmail.com>
2023-11-23 23:04:16 +0100

    read test cypress

--- cookblog-frontend/cypress/e2e/recipe.spec.cy.ts
+++ cookblog-frontend/cypress/e2e/recipe.spec.cy.ts
@@ -89,6 +89,55 @@ describe('Recipe Tests', () => {
     cy.visit('/');
   });
 
+  it('Should edit recipe', () => {
+    cy.fixture('recipe').then((recipe) => {
+      cy.visit('/');
+      clickNextUntilDisabled();
+      cy.get('.button').contains('Edit').click({ force: true });
+      cy.wait(1000);
+      cy.get('input[data-test-id="title"]').type(recipe.title + 'edited');
+      cy.get('textarea[data-test-id="description"]').type(
+        recipe.description + 'edited',
+      );
+      cy.get('textarea[data-test-id="instructions"]').type(
+        recipe.instructions + 'edited',
+      );
+      cy.get('input[data-test-id="image"]').type(recipe.image + 'edited');
+
+      cy.get('mat-select[data-test-id="category"]').click();
+      cy.get('mat-option').contains(categoryName).click();
+
+      cy.get('input[data-test-id="preparationTimeInMinutes"]').type(
+        recipe.preparationTimeInMinutes,
+        { force: true },
+      );
+
+      cy.get('input[data-test-id="portions"]').type(recipe.portions + 1);
+      cy.get('input[data-test-id="calorificValue"]').type(
+        recipe.calorificValue + 1,
+        {
+          force: true,
+        },
+      );
+      cy.get('input[data-test-id="ingredient-name"]').type(
+        recipe.calorificValue + 1,
+      );
+
+      cy.get('button[aria-label="save"]').click();
+      cy.visit('/');
+
+      clickNextUntilDisabled();
+      cy.get('.button').contains('Read').click({ force: true });
+      cy.wait(1000);
+      cy.contains(recipe.title).should('exist');
+      cy.contains(recipe.description).should('exist');
+      cy.contains(recipe.instructions).should('exist');
+      cy.contains(recipe.preparationTimeInMinutes).should('exist');
+      cy.contains(recipe.calorificValue).should('exist');
+      cy.contains(recipe.portions).should('exist');
+    });
+  });
+
   it('Should delete recipe ', () => {
     cy.fixture('recipe').then((recipe) => {
       cy.visit('/');
@@ -104,13 +153,5 @@ describe('Recipe Tests', () => {
       clickNextUntilDisabled();
       cy.contains('.card', recipe.title).should('not.exist');
     });
-  });
-
-  it('There should be recipes', () => {
-    cy.visit('/');
-  });
-
-  it('Should edit recipe', () => {
-    cy.visit('/');
   });
 });
