fc36d80a812fc167a0a734df965601ebe7289d6a
Milosz Mertka <milosz.mertka@onet.pl>
2023-11-25 22:56:08 +0100

    Fixed E2E tests

--- cookblog-frontend/cypress/e2e/recipes.spec.cy.ts
+++ cookblog-frontend/cypress/e2e/recipes.spec.cy.ts
@@ -8,24 +8,37 @@ describe('Recipes Tests', () => {
   it('Should set the category of a recipe', () => {
     cy.visit('/recipes/edit/1');
     cy.wait(1000);
-    cy.get('mat-select[data-test-id="category"]').click().get('mat-option').last().click();
+    cy.get('mat-select[data-test-id="category"]')
+      .click()
+      .get('mat-option')
+      .last()
+      .click();
     cy.get('button[aria-label="save"]').click();
     cy.wait(1000);
     cy.visit('/recipes/edit/1');
     cy.wait(1000);
-    cy.get('mat-select[data-test-id="category"]').click().get('mat-option').last().should('have.attr', 'aria-selected', 'true');
+    cy.get('mat-select[data-test-id="category"]')
+      .click()
+      .get('mat-option')
+      .last()
+      .should('have.attr', 'aria-selected', 'true');
   });
 
   it('Should add information about calorific value', () => {
     cy.fixture('recipe').then((recipe) => {
       cy.visit('/recipes/edit/1');
       cy.wait(1000);
-      cy.get('input[data-test-id="calorificValue"]').clear().type(recipe.calorificValue);
+      cy.get('input[data-test-id="calorificValue"]')
+        .clear()
+        .type(recipe.calorificValue);
       cy.get('button[aria-label="save"]').click();
       cy.wait(1000);
       cy.visit('/recipes/edit/1');
       cy.wait(1000);
-      cy.get('input[data-test-id="calorificValue"]').should('have.value', recipe.calorificValue);
+      cy.get('input[data-test-id="calorificValue"]').should(
+        'have.value',
+        recipe.calorificValue,
+      );
     });
   });
 
@@ -33,12 +46,17 @@ describe('Recipes Tests', () => {
     cy.fixture('recipe').then((recipe) => {
       cy.visit('/recipes/edit/1');
       cy.wait(1000);
-      cy.get('input[data-test-id="preparationTimeInMinutes"]').clear().type(recipe.preparationTimeInMinutes);
+      cy.get('input[data-test-id="preparationTimeInMinutes"]')
+        .clear()
+        .type(recipe.preparationTimeInMinutes);
       cy.get('button[aria-label="save"]').click();
       cy.wait(1000);
       cy.visit('/recipes/edit/1');
       cy.wait(1000);
-      cy.get('input[data-test-id="preparationTimeInMinutes"]').should('have.value', recipe.preparationTimeInMinutes);
+      cy.get('input[data-test-id="preparationTimeInMinutes"]').should(
+        'have.value',
+        recipe.preparationTimeInMinutes,
+      );
     });
   });
 
@@ -51,7 +69,10 @@ describe('Recipes Tests', () => {
       cy.wait(1000);
       cy.visit('/recipes/edit/1');
       cy.wait(1000);
-      cy.get('input[data-test-id="portions"]').should('have.value', recipe.portions);
+      cy.get('input[data-test-id="portions"]').should(
+        'have.value',
+        recipe.portions,
+      );
     });
   });
 
@@ -60,14 +81,23 @@ describe('Recipes Tests', () => {
       cy.visit('/recipes/edit/1');
       cy.wait(1000);
       cy.contains('Add ingredient').click();
-      cy.get('input[data-test-id="ingredient-name"]').last().type(recipe.ingredientName);
-      cy.get('input[data-test-id="ingredient-amount"]').last().clear().type(recipe.amount);
+      cy.get('input[data-test-id="ingredient-name"]')
+        .last()
+        .type(recipe.ingredientName);
+      cy.get('input[data-test-id="ingredient-amount"]')
+        .last()
+        .clear()
+        .type(recipe.amount);
       cy.get('button[aria-label="save"]').click();
       cy.wait(1000);
       cy.visit('/recipes/edit/1');
       cy.wait(1000);
-      cy.get('input[data-test-id="ingredient-name"]').eq(1).should('have.value', recipe.ingredientName);
-      cy.get('input[data-test-id="ingredient-amount"]').eq(1).should('have.value', recipe.amount);
+      cy.get('input[data-test-id="ingredient-name"]')
+        .eq(0)
+        .should('have.value', recipe.ingredientName);
+      cy.get('input[data-test-id="ingredient-amount"]')
+        .eq(0)
+        .should('have.value', recipe.amount);
     });
   });
 });
