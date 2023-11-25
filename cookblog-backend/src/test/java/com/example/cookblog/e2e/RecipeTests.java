6a3d67d712621b303774bc60359c8bd9c948a92f
bborkowsp <pawel.borkowski0704@gmail.com>
2023-11-25 14:32:14 +0100

    refactor

--- cookblog-backend/src/test/java/com/example/cookblog/e2e/RecipeTests.java
+++ cookblog-backend/src/test/java/com/example/cookblog/e2e/RecipeTests.java
@@ -28,7 +28,6 @@ public class RecipeTests {
 
         while (true) {
             var nextPageButton = driver.findElement(By.id("nextPageButton"));
-            Thread.sleep(1000);
             if (nextPageButton.getAttribute("disabled") != null && nextPageButton.getAttribute("disabled").equals("true")) {
                 Thread.sleep(1000);
                 break;
@@ -40,16 +39,13 @@ public class RecipeTests {
 
     private void clickNextPage() throws InterruptedException {
         final var js = (JavascriptExecutor) driver;
-
         var nextPageButton = driver.findElement(By.id("nextPageButton"));
-        Thread.sleep(1000);
-
         if (nextPageButton.getAttribute("disabled") == null || !nextPageButton.getAttribute("disabled").equals("true")) {
             Thread.sleep(1000);
             js.executeScript("arguments[0].click();", nextPageButton);
         }
+        Thread.sleep(1000);
     }
-
 
     @Test
     @Order(1)
@@ -62,6 +58,7 @@ public class RecipeTests {
         final var imagePath = "https://images.unsplash.com/photo-1476224203421-9ac39bcb3327?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
         final var ingredientName = "myingredient";
         final var calorificValue = "3";
+
         driver.findElement(By.cssSelector("a[aria-label='create recipe']")).click();
 
         driver.findElement(By.cssSelector("input[data-test-id='title']")).sendKeys(recipeTitle);
@@ -73,7 +70,7 @@ public class RecipeTests {
         final var categoryDropDown = driver.findElement(By.cssSelector("mat-select[data-test-id='category']"));
         categoryDropDown.click();
         Thread.sleep(1000);
-        driver.findElement(By.id("mat-option-5")).click();
+        driver.findElement(By.xpath("//mat-option/span[contains(text(),'libero')]")).click();
 
         driver.findElement(By.cssSelector("input[data-test-id='preparationTimeInMinutes']")).sendKeys(preparationTimeInMinutes);
         driver.findElement(By.cssSelector("input[data-test-id='portions']")).sendKeys(portions);
@@ -82,6 +79,7 @@ public class RecipeTests {
 
         driver.findElement(By.cssSelector("button[aria-label='save']")).click();
         Thread.sleep(1000);
+        driver.navigate().to(APP_URL);
         clickNextUntilDisabled();
 
         final var titleElement = driver.findElement(By.xpath("//*[contains(text(),'" + recipeTitle + "')]"));
@@ -115,7 +113,8 @@ public class RecipeTests {
         assertThat(ingredientNameElement.isDisplayed()).isTrue();
         final var instructionsElement = driver.findElement(By.xpath("//*[contains(text(),'" + recipeInstructions + "')]"));
         assertThat(instructionsElement.isDisplayed()).isTrue();
-
+        final var imageElement = driver.findElement(By.xpath("//img[contains(@src,'" + imagePath + "')]"));
+        assertThat(imageElement.isDisplayed()).isTrue();
         final var preparationTimeInMinutesElement = driver.findElement(By.xpath("//*[contains(text(),'" + preparationTimeInMinutes + " min." + "')]"));
         assertThat(preparationTimeInMinutesElement.isDisplayed()).isTrue();
         final var calorificValueElement = driver.findElement(By.xpath("//*[contains(text(),'" + calorificValue + " kcal" + "')]"));
@@ -133,6 +132,7 @@ public class RecipeTests {
         final var recipeDescriptionSecondPage = "Omnis error aperiam. Veritatis id ullam sit numquam qui et nulla. Odio maxime adipisci velit. Vitae ut corrupti est provident. Aliquid non temporibus nihil dolores.";
         final var recipeTitleThirdPage = "Necessitatibus enim quasi animi facere ut quos.";
         final var recipeDescriptionThirdPage = "Temporibus nostrum ratione aut laborum molestiae quia quod. Qui fuga et laboriosam praesentium eum. Earum ad architecto hic fugit reiciendis. Beatae ad voluptas. Nostrum odit totam.";
+
         driver.navigate().to(APP_URL);
         Thread.sleep(1000);
 
@@ -141,23 +141,17 @@ public class RecipeTests {
         final var recipeDescriptionFirstPageElement = driver.findElement(By.xpath("//*[contains(text(),'" + recipeDescriptionFirstPage + "')]"));
         assertThat(recipeDescriptionFirstPageElement.isDisplayed()).isTrue();
         clickNextPage();
-        Thread.sleep(1000);
 
         final var recipeTitleSecondPageElement = driver.findElement(By.xpath("//*[contains(text(),'" + recipeTitleSecondPage + "')]"));
         assertThat(recipeTitleSecondPageElement.isDisplayed()).isTrue();
         final var recipeDescriptionSecondPageElement = driver.findElement(By.xpath("//*[contains(text(),'" + recipeDescriptionSecondPage + "')]"));
         assertThat(recipeDescriptionSecondPageElement.isDisplayed()).isTrue();
         clickNextPage();
-        Thread.sleep(1000);
 
         final var recipeTitleThirdPageElement = driver.findElement(By.xpath("//*[contains(text(),'" + recipeTitleThirdPage + "')]"));
         assertThat(recipeTitleThirdPageElement.isDisplayed()).isTrue();
-        Thread.sleep(1000);
-
         final var recipeDescriptionThirdPageElement = driver.findElement(By.xpath("//*[contains(text(),'" + recipeDescriptionThirdPage + "')]"));
-        Thread.sleep(1000);
         assertThat(recipeDescriptionThirdPageElement.isDisplayed()).isTrue();
-
     }
 
     @Test
@@ -170,47 +164,67 @@ public class RecipeTests {
         final var portionsEdited = "82";
         final var imagePathEdited = "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
         final var calorificValueEdited = "73";
+        final var ingredientNameEdited = "EditedIngredient";
+        final var amountEdited = "62";
+
         driver.navigate().to(APP_URL);
         Thread.sleep(1000);
         clickNextUntilDisabled();
         Thread.sleep(1000);
+
         final var editButton = driver.findElement(By.cssSelector("a[aria-label='editRecipeButton']"));
         editButton.click();
 
         final var titleInput = driver.findElement(By.cssSelector("input[data-test-id='title']"));
         titleInput.clear();
         titleInput.sendKeys(recipeTitleEdited);
-        Thread.sleep(500);
-
+        Thread.sleep(300);
 
         final var descriptionInput = driver.findElement(By.cssSelector("textarea[data-test-id='description']"));
         descriptionInput.clear();
         descriptionInput.sendKeys(recipeDescriptionEdited);
-        Thread.sleep(500);
+        Thread.sleep(300);
 
         final var instructionsInput = driver.findElement(By.cssSelector("textarea[data-test-id='instructions']"));
         instructionsInput.clear();
         instructionsInput.sendKeys(recipeInstructionsEdited);
-        Thread.sleep(500);
+        Thread.sleep(300);
 
         final var imageInput = driver.findElement(By.cssSelector("input[data-test-id='image']"));
         imageInput.clear();
         imageInput.sendKeys(imagePathEdited);
-        Thread.sleep(500);
+        Thread.sleep(300);
+
+        Thread.sleep(1000);
+        final var categoryDropDown = driver.findElement(By.cssSelector("mat-select[data-test-id='category']"));
+        categoryDropDown.click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//mat-option/span[contains(text(),'dicta')]")).click();
 
         final var preparationTimeInput = driver.findElement(By.cssSelector("input[data-test-id='preparationTimeInMinutes']"));
         preparationTimeInput.clear();
         preparationTimeInput.sendKeys(preparationTimeInMinutesEdited);
-        Thread.sleep(500);
+        Thread.sleep(300);
 
         final var portionsInput = driver.findElement(By.cssSelector("input[data-test-id='portions']"));
         portionsInput.clear();
         portionsInput.sendKeys(portionsEdited);
-        Thread.sleep(500);
+        Thread.sleep(300);
 
         final var calorificValueInput = driver.findElement(By.cssSelector("input[data-test-id='calorificValue']"));
         calorificValueInput.clear();
         calorificValueInput.sendKeys(calorificValueEdited);
+        Thread.sleep(300);
+
+        final var ingredientNameInput = driver.findElement(By.cssSelector("input[data-test-id='ingredient-name']"));
+        ingredientNameInput.clear();
+        ingredientNameInput.sendKeys(ingredientNameEdited);
+        Thread.sleep(300);
+
+        final var amountValueInput = driver.findElement(By.cssSelector("input[data-test-id=\"ingredient-amount\"]"));
+        amountValueInput.clear();
+        amountValueInput.sendKeys(amountEdited);
+        Thread.sleep(300);
 
         Thread.sleep(1000);
         driver.findElement(By.cssSelector("button[aria-label='save']")).click();
@@ -226,18 +240,20 @@ public class RecipeTests {
         assertThat(titleElement.isDisplayed()).isTrue();
         final var recipeDescriptionElement = driver.findElement(By.xpath("//*[contains(text(),'" + recipeDescriptionEdited + "')]"));
         assertThat(recipeDescriptionElement.isDisplayed()).isTrue();
-
         final var instructionsElement = driver.findElement(By.xpath("//*[contains(text(),'" + recipeInstructionsEdited + "')]"));
         assertThat(instructionsElement.isDisplayed()).isTrue();
-
         final var preparationTimeInMinutesElement = driver.findElement(By.xpath("//*[contains(text(),'" + preparationTimeInMinutesEdited + " min." + "')]"));
         assertThat(preparationTimeInMinutesElement.isDisplayed()).isTrue();
         final var calorificValueElement = driver.findElement(By.xpath("//*[contains(text(),'" + calorificValueEdited + " kcal" + "')]"));
         assertThat(calorificValueElement.isDisplayed()).isTrue();
         final var portionsElement = driver.findElement(By.xpath("//*[contains(normalize-space(),'" + portionsEdited + "')]"));
         assertThat(portionsElement.isDisplayed()).isTrue();
-        Thread.sleep(1000);
-
+        final var ingredientElement = driver.findElement(By.xpath("//*[contains(normalize-space(),'" + ingredientNameEdited + "')]"));
+        assertThat(ingredientElement.isDisplayed()).isTrue();
+        final var amountElement = driver.findElement(By.xpath("//*[contains(normalize-space(),'" + amountEdited + "')]"));
+        assertThat(amountElement.isDisplayed()).isTrue();
+        final var imageElement = driver.findElement(By.xpath("//img[contains(@src,'" + imagePathEdited + "')]"));
+        assertThat(imageElement.isDisplayed()).isTrue();
     }
 
     @Test
@@ -245,16 +261,18 @@ public class RecipeTests {
     public void givenRecipe_WhenDeleteRecipe_ThenRecipeIsRemoved() throws InterruptedException {
         driver.navigate().to(APP_URL);
         clickNextUntilDisabled();
-        final var recipeTitle = "title";
-        final var menuButton = driver.findElement(By.cssSelector("button[aria-label='deleteRecipeButton']"));
-        menuButton.click();
+
+        final var recipeTitleEdited = "Editedmytitle";
+        final var deleteButton = driver.findElement(By.cssSelector("button[aria-label='deleteRecipeButton']"));
+        deleteButton.click();
+
         Thread.sleep(1000);
         final var js = (JavascriptExecutor) driver;
         js.executeScript("document.querySelector('[data-test-id=\"confirm-button\"]').click();");
         Thread.sleep(1000);
+
         driver.navigate().to(APP_URL);
         clickNextUntilDisabled();
-        String actualText = driver.findElement(By.xpath("//*[contains(text(),'" + recipeTitle + "')]")).getText().trim();
-        assertThat(actualText.isEmpty()).isTrue();
+        assertThat(driver.findElements(By.xpath("//*[contains(text(),'" + recipeTitleEdited + "')]")).isEmpty()).isTrue();
     }
 }
