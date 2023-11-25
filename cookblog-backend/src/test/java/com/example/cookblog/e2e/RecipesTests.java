a09c7d4d1aff9bd9360f31ebfe7ce58509567624
Stanisław Maliński <95365847+Stanleyowskiki@users.noreply.github.com>
2023-11-25 21:28:24 +0100

    God save the queen

--- cookblog-backend/src/test/java/com/example/cookblog/e2e/RecipesTests.java
+++ cookblog-backend/src/test/java/com/example/cookblog/e2e/RecipesTests.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.util.Objects;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @TestMethodOrder(OrderAnnotation.class)
@@ -98,21 +100,75 @@ public class RecipesTests {
         driver.get(APP_URL + "/recipes/edit/1");
         Thread.sleep(1000);
         portionsInput = driver.findElement(By.cssSelector("input[data-test-id='portions']"));
-        assertThat(portionsInput.getAttribute("value")).isEqualTo(portions)\;
+        assertThat(portionsInput.getAttribute("value")).isEqualTo(portions);
     }
 
     @Test
     public void givenRecipeData_whenAddInformationAboutPhotoUrl_shouldSuccessfullyAddPhoto() throws Exception {
-        // TODO Dodawnie zdjęć
+        String photoUrl = "PhotoUrl";
+        driver.get(APP_URL + "/recipes/edit/1");
+        Thread.sleep(1000);
+
+        var imageInput = driver.findElement(By.cssSelector("input[data-test-id='image']"));
+        imageInput.clear();
+        imageInput.sendKeys(photoUrl);
+        final var saveButton = driver.findElement(By.cssSelector("button[aria-label='save']"));
+        saveButton.click();
+        Thread.sleep(1000);
+
+        driver.get(APP_URL + "/recipes/edit/1");
+        Thread.sleep(1000);
+
+        imageInput = driver.findElement(By.cssSelector("input[data-test-id='image']"));
+        assertThat(imageInput.getAttribute("value")).isEqualTo(photoUrl);
     }
 
     @Test
     public void givenRecipeQuery_whenSearchedWithQuery_shouldShowResultOfTheSearch() throws Exception {
-        // TODO Wyszukiwanie przepisów
+        String recipeName = "Name";
+        driver.get(APP_URL + "/recipes/edit/1");
+        Thread.sleep(1000);
+
+        var tilteInput = driver.findElement(By.cssSelector("input[data-test-id='title']"));
+        tilteInput.clear();
+        tilteInput.sendKeys(recipeName);
+        final var saveButton = driver.findElement(By.cssSelector("button[aria-label='save']"));
+        saveButton.click();
+        Thread.sleep(1000);
+
+        driver.get(APP_URL + "/recipes/edit/1");
+        Thread.sleep(1000);
+
+        final var element = driver.findElement(By.xpath("//*[contains(text(),'" + recipeName + "')]"));
+        assertThat(element.isDisplayed()).isTrue();
     }
 
     @Test
-    public void givenRecipeData_whenRequestCategoryForRecipe_shouldProperlyRetrieveCategory() throws Exception {
-        // TODO Pobieranie kategorii przepisu
+    public void givenRecipeData_whenRequestRecipeForCategory_shouldProperlyRetrieveRecipeOfThatCategory() throws Exception {
+        String recipeName = "Name";
+        driver.get(APP_URL + "/recipes/edit/1");
+        Thread.sleep(1000);
+
+        var tilteInput = driver.findElement(By.cssSelector("input[data-test-id='title']"));
+        tilteInput.clear();
+        tilteInput.sendKeys(recipeName);
+
+        var categorySelect = driver.findElement(By.cssSelector("mat-select[data-test-id='category']"));
+        categorySelect.click();
+        var lastMatOption = driver.findElements(By.cssSelector("mat-option")).get(driver.findElements(By.cssSelector("mat-option")).size() - 1);
+        lastMatOption.click();
+        lastMatOption.click();
+        final var saveButtonRec = driver.findElement(By.cssSelector("button[aria-label='save']"));
+        saveButtonRec.click();
+        Thread.sleep(1000);
+
+        driver.get(APP_URL + "/categories/1");
+        Thread.sleep(1000);
+
+
+
+        final var saveButton = driver.findElement(By.cssSelector("button[aria-label='save']"));
+        saveButton.click();
+        Thread.sleep(1000);
     }
 }
