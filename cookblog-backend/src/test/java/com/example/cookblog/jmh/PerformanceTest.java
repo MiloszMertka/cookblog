c9d02247258aca5cf4ace2764fdca3db8ab83c3c
bborkowsp <pawel.borkowski0704@gmail.com>
2023-11-26 14:02:00 +0100

    jmh

---
+++ cookblog-backend/src/test/java/com/example/cookblog/jmh/PerformanceTest.java
@@ -0,0 +1,52 @@
+package com.example.cookblog.jmh;
+
+import com.example.cookblog.CookblogApplication;
+import com.example.cookblog.dto.requests.CreateCategoryRequest;
+import com.example.cookblog.service.CategoryUseCases;
+import org.junit.Test;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+public class PerformanceTest {
+
+    private CookblogApplication app;
+    private ConfigurableApplicationContext context;
+    private CategoryUseCases categoryService;
+
+    @Test
+    public void contextLoads() throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(PerformanceTest.class.getSimpleName())
+                .forks(1)
+                .build();
+        new Runner(opt).run();
+    }
+
+    @Setup
+    public void setup() {
+        this.context = new SpringApplication(CookblogApplication.class).run();
+        categoryService = this.context.getBean(CategoryUseCases.class);
+    }
+
+    @TearDown
+    public void tearDown() {
+        this.context.close();
+    }
+
+    @Benchmark
+    @Warmup(iterations = 0)
+    public void createCategoryBenchmark() {
+        String uniqueCategoryName = "category" + System.currentTimeMillis();
+
+        categoryService.createCategory(CreateCategoryRequest.builder()
+                .name(uniqueCategoryName)
+                .build());
+    }
+}
