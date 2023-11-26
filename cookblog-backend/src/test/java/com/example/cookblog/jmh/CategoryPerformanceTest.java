dba01d4909dda35343bfcfb48da0d3c459a4c4df
Kamil Jabłkowski <kamil.jablkowski5@gmail.com>
2023-11-26 17:41:02 +0100

    Updated

--- cookblog-backend/src/test/java/com/example/cookblog/jmh/CategoryPerformanceTest.java
+++ cookblog-backend/src/test/java/com/example/cookblog/jmh/CategoryPerformanceTest.java
@@ -2,15 +2,28 @@ package com.example.cookblog.jmh;
 
 import com.example.cookblog.CookblogApplication;
 import com.example.cookblog.dto.requests.CreateCategoryRequest;
+import com.example.cookblog.dto.requests.CreateRecipeRequest;
+import com.example.cookblog.dto.resources.ImageResource;
+import com.example.cookblog.dto.resources.IngredientResource;
+import com.example.cookblog.dto.resources.QuantityResource;
+import com.example.cookblog.mapper.CategoryMapper;
+import com.example.cookblog.mapper.RecipeMapper;
+import com.example.cookblog.model.*;
+import com.example.cookblog.repository.CategoryRepository;
+import com.example.cookblog.repository.RecipeRepository;
 import com.example.cookblog.service.CategoryUseCases;
+import com.example.cookblog.service.RecipeUseCases;
 import org.junit.Test;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Set;
 
 @State(Scope.Benchmark)
 @BenchmarkMode(Mode.AverageTime)
@@ -18,6 +31,14 @@ public class CategoryPerformanceTest {
 
     private ConfigurableApplicationContext context;
     private CategoryUseCases categoryService;
+    private RecipeUseCases recipeService;
+    private CategoryRepository categoryRepository;
+    private RecipeRepository recipeRepository;
+    private CategoryMapper categoryMapper;
+    private RecipeMapper recipeMapper;
+
+    private Recipe recipe;
+    private Category category;
 
     @Test
     public void contextLoads() throws RunnerException {
@@ -31,7 +52,17 @@ public class CategoryPerformanceTest {
     @Setup
     public void setup() {
         this.context = new SpringApplication(CookblogApplication.class).run();
+
         categoryService = this.context.getBean(CategoryUseCases.class);
+        recipeService = this.context.getBean(RecipeUseCases.class);
+        categoryRepository = this.context.getBean(CategoryRepository.class);
+        recipeRepository = this.context.getBean(RecipeRepository.class);
+        categoryMapper = this.context.getBean(CategoryMapper.class);
+        recipeMapper = this.context.getBean(RecipeMapper.class);
+
+        category = Category.builder()
+                .name("category")
+                .build();
     }
 
     @TearDown
@@ -48,4 +79,27 @@ public class CategoryPerformanceTest {
                 .name(uniqueCategoryName)
                 .build());
     }
+
+    @Benchmark
+    @Warmup(iterations = 0)
+    public void createRecipeBenchmark() {
+        recipeService.createRecipe(CreateRecipeRequest.builder()
+                .title("title")
+                .description("description")
+                .instructions("instructions")
+                .ingredients(Set.of(IngredientResource.builder()
+                        .name("ingredient")
+                        .quantity(QuantityResource.builder()
+                                .amount(1)
+                                .unit(Unit.GRAM)
+                                .build())
+                        .build()))
+                .image(ImageResource.builder()
+                        .path("image-path")
+                        .build())
+                .category(null)
+                .build());
+    }
+
+
 }
